a98c9c8345679c9eced1f55f4ec0d9947e6f844b
Yassine Achouyne <yassine.achouyne2022@gmail.com>
2022-06-24 16:44:35 +0100

    24/6

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function césar_chiffrement(){
     var textareainput=document.getElementById('textareainput').value;
     var textareaoutput=document.getElementById('textareaoutput');
     var tab=[];
-    var k=0;
+    var k=Number(cle.value);;
     for(i=0;i<abc.length;i++){
         if(i+Number(cle.value)>abc.length){
             k++
@@ -17,9 +17,44 @@ function césar_chiffrement(){
     }
     var text='';
     for(i=0;i<textareainput.length;i++){
+        if(textareainput==" ")continue; 
+        for(j=0;j<abc.length;j++){
+            if(textareainput[i]==abc[j]){
+                var pos=j
+            }
+        }
+        text+=tab[pos]
+    }
+    textareaoutput.innerHTML=text ;
+}
+
+function césar_dechiffrement(){
+    var abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var cle=document.getElementById('clecesar');
+    var textareainput=document.getElementById('textareainput').value;
+    var textareaoutput=document.getElementById('textareaoutput');
+    var chiff=abc
+    var tab=[];
+    var k=Number(cle.value);
+    for(i=0;i<abc.length;i++){
+        if(i<Number(cle.value)){
+            chiff.unshift(abc[abc.length-i-1])
+        }else{
+
+        }
+    }
+    for(i=0 ; i<26; i++){
+        tab.push(chiff[i])
+    }
+    var text='';
+    for(i=0;i<textareainput.length;i++){
         if(textareainput==" ")continue;
-        text+=tab[i]
-        
+        for(j=0;j<abc.length;j++){
+            if(textareainput[i]==abc[j]){
+                var pos=j-k
+            }
+        }
+        text+=tab[pos]
     }
     textareaoutput.innerHTML=text;
 }
